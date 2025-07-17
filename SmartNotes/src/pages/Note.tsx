ba474b875fc8fc862fb1903f23fff794396e8675
Fernando <FernandoLpz0911.business@gmail.com>
2025-07-17 12:55:43 -0500

    Added text boxes and a back button for notes app

--- SmartNotes/src/pages/Note.tsx
+++ SmartNotes/src/pages/Note.tsx
@@ -1,13 +1,22 @@
+import "./cssfiles/Note.css";
+import { Link } from 'react-router-dom';
+
 function Note() {
     return (
         <>
-            <div id="root">
-                <input type="text" id="note" name="note"></input>
-                <div className="note">
-                    <button>
-                    count is 
-                    </button>
-                </div>
+            <div className="note-topbar">
+                <button className="back-button">
+                    <Link to="/Home"> Home/Submit Note to Server</Link>
+                </button>
+                <input type="text" className="note-title" id="note-title" name="title" placeholder="Title">
+                
+                </input>
+            </div>
+
+            <div className="page-note">
+                <input type="text" id="note" name="note" placeholder="New Note">
+                
+                </input>
             </div>
         </>
     )
